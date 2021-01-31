5d96f18b144085a944fc80117090bc2b938d42cc
Oliver Leppänen <oliver.leppanen@koherent.io>
2021-01-31 14:59:38 +0200

    Exercise 1.07

--- main-application/main.go
+++ main-application/main.go
@@ -2,14 +2,51 @@ package main
 
 import(
 	"log"
+	"fmt"
 	"time"
 	"math/rand"
+	"net/http"
+	"io"
+	"os"
+	"os/signal"
+	"syscall"
 )
+
+var str string
+
+func getStatus(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/status" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		io.WriteString(w, fmt.Sprintf("%s %s", time.Now(), str))
+	}
+}
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	str := generateString(16)
+	str = generateString(16)
+	port := 3000
+
+	http.HandleFunc("/status", getStatus)
+
+	log.Printf("Listening to port %v\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		os.Exit(1)
+	}()
 
 	for {
 		// Log automatically prints out the timestamp
